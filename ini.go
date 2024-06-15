c3f239decf4b0792edb9285a4402c0b8227084aa
Matthias Watermann <support@mwat.de>
2024-06-15 20:21:51 +0200

    * move `ReadIniData()` from `section.go` to `ini.go`;

--- ini.go
+++ ini.go
@@ -6,6 +6,11 @@ Copyright © 2019, 2024  M.Watermann, 10247 Berlin, Germany
 */
 package ini
 
+import (
+	"os"
+	"path/filepath"
+)
+
 //lint:file-ignore ST1017 - I prefer Yoda conditions
 
 // `New()` reads the given `aFilename` returning the data structure read
@@ -24,5 +29,85 @@ func New(aFilename string) (*TSectionList, error) {
 	}
 	return result.Load()
 } // New()
+
+// `ReadIniData()` returns the config values read from INI file(s).
+//
+//	The steps here are:
+//	(1) read the local `./.aName.ini`,
+//	(2) read the global `/etc/aName.ini`,
+//	(3) read the user-local `~/.aName.ini`,
+//	(4) read the user-local `~/.config/aName.ini`,
+//	(5) read the `-ini` commandline argument.
+//
+// This function considers only the `Default` section of the INI files.
+//
+// Example:
+//
+//	iniData, _ := ReadIniData("myApp")
+//	fmt.Println(iniData.AsString("myKey"))
+//
+// The function returns a pointer to the 'Default' section
+// of the first INI file that contains it.
+//
+//	`aName` The application's name used as the INI file name (without extension).
+func ReadIniData(aName string) *TSection {
+	var (
+		confDir    string
+		err        error
+		ini1, ini2 *TSectionList
+	)
+	// (1) ./
+	fName, _ := filepath.Abs(`./` + aName + `.ini`)
+	if ini1, err = New(fName); nil == err {
+		ini1.AddSectionKey(ini1.defSect, `iniFile`, fName)
+	}
+
+	// (2) /etc/
+	fName = `/etc/` + aName + `.ini`
+	if ini2, err = New(fName); nil == err {
+		ini1.Merge(ini2)
+		ini1.AddSectionKey(ini1.defSect, `iniFile`, fName)
+	}
+
+	// (3) ~user/
+	fName, err = os.UserHomeDir()
+	if (nil == err) && (0 < len(fName)) {
+		fName, _ = filepath.Abs(filepath.Join(fName, `.`+aName+`.ini`))
+		if ini2, err = New(fName); nil == err {
+			ini1.Merge(ini2)
+			ini1.AddSectionKey(ini1.defSect, `iniFile`, fName)
+		}
+	}
+
+	// (4) ~/.config/
+	if confDir, err = os.UserConfigDir(); nil == err {
+		fName, _ = filepath.Abs(filepath.Join(confDir, aName+`.ini`))
+		if ini2, err = New(fName); nil == err {
+			ini1.Merge(ini2)
+			ini1.AddSectionKey(ini1.defSect, `iniFile`, fName)
+		}
+	}
+
+	// (5) cmdline
+	aLen := len(os.Args)
+	for i := 1; i < aLen; i++ {
+		if `-ini` == os.Args[i] {
+			//XXX Note that this works only if `-ini` and
+			// filename are two separate arguments. It will
+			// fail if it's given in the form `-ini=filename`.
+			i++
+			if i < aLen {
+				fName, _ = filepath.Abs(os.Args[i])
+				if ini2, err = New(fName); nil == err {
+					ini1.Merge(ini2)
+					ini1.AddSectionKey(ini1.defSect, `iniFile`, fName)
+				}
+			}
+			break
+		}
+	}
+
+	return ini1.GetSection(ini1.defSect)
+} // ReadIniData()
 
 /* _EoF_ */
--- section.go
+++ section.go
@@ -6,11 +6,6 @@ Copyright © 2019, 2024  M.Watermann, 10247 Berlin, Germany
 */
 package ini
 
-import (
-	"os"
-	"path/filepath"
-)
-
 //lint:file-ignore ST1017 - I prefer Yoda conditions
 
 type (
@@ -400,94 +395,5 @@ func (ks *TSection) UpdateKey(aKey, aValue string) bool {
 	// if aKey doesn't exist then create a new entry
 	return ks.AddKey(aKey, aValue)
 } // UpdateKey()
-
-// `ReadIniData()` returns the config values read from INI file(s).
-//
-//	The steps here are:
-//	(1) read the local `./.aName.ini`,
-//	(2) read the global `/etc/aName.ini`,
-//	(3) read the user-local `~/.aName.ini`,
-//	(4) read the user-local `~/.config/aName.ini`,
-//	(5) read the `-ini` commandline argument.
-//
-// This function considers only the `Default` section of the INI files.
-//
-// The function returns a pointer to the 'Default' section of the
-// first INI file that contains it. If no 'Default' section is found
-// in any of the INI files, the function returns nil.
-//
-// Example:
-//
-// 	iniData, _ := ReadIniData("myApp")
-// 	fmt.Println(iniData.AsString("myKey"))
-//
-// The function returns two values: a pointer to the 'Default' section
-// of the first INI file that contains it, and an error value.
-//
-// The error value is nil if no error occurred. Otherwise, it contains
-// an error message that describes the problem.
-
-//	`aName` The application's name used in the INI file name (without
-// `.ini` extension).
-
-func ReadIniData(aName string) *TSection {
-	var (
-		confDir    string
-		err        error
-		ini1, ini2 *TSectionList
-	)
-	// (1) ./
-	fName, _ := filepath.Abs(`./` + aName + `.ini`)
-	if ini1, err = New(fName); nil == err {
-		ini1.AddSectionKey(ini1.defSect, `iniFile`, fName)
-	}
-
-	// (2) /etc/
-	fName = `/etc/` + aName + `.ini`
-	if ini2, err = New(fName); nil == err {
-		ini1.Merge(ini2)
-		ini1.AddSectionKey(ini1.defSect, `iniFile`, fName)
-	}
-
-	// (3) ~user/
-	fName, err = os.UserHomeDir()
-	if (nil == err) && (0 < len(fName)) {
-		fName, _ = filepath.Abs(filepath.Join(fName, `.`+aName+`.ini`))
-		if ini2, err = New(fName); nil == err {
-			ini1.Merge(ini2)
-			ini1.AddSectionKey(ini1.defSect, `iniFile`, fName)
-		}
-	}
-
-	// (4) ~/.config/
-	if confDir, err = os.UserConfigDir(); nil == err {
-		fName, _ = filepath.Abs(filepath.Join(confDir, aName+`.ini`))
-		if ini2, err = New(fName); nil == err {
-			ini1.Merge(ini2)
-			ini1.AddSectionKey(ini1.defSect, `iniFile`, fName)
-		}
-	}
-
-	// (5) cmdline
-	aLen := len(os.Args)
-	for i := 1; i < aLen; i++ {
-		if `-ini` == os.Args[i] {
-			//XXX Note that this works only if `-ini` and
-			// filename are two separate arguments. It will
-			// fail if it's given in the form `-ini=filename`.
-			i++
-			if i < aLen {
-				fName, _ = filepath.Abs(os.Args[i])
-				if ini2, err = New(fName); nil == err {
-					ini1.Merge(ini2)
-					ini1.AddSectionKey(ini1.defSect, `iniFile`, fName)
-				}
-			}
-			break
-		}
-	}
-
-	return ini1.GetSection(ini1.defSect)
-} // ReadIniData()
 
 /* _EoF_ */
