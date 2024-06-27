d6136853b33d3cdbaddb8c8e3e8763258378536d
Matthias Watermann <support@mwat.de>
2024-06-27 21:37:12 +0200

    + add another unit-test;

--- ini_test.go
+++ ini_test.go
@@ -13,6 +13,31 @@ import (
 
 //lint:file-ignore ST1017 - I prefer Yoda conditions
 
+func TestNewIni(t *testing.T) {
+	fName := "testIn.ini"
+
+	tests := []struct {
+		name     string
+		filename string
+		// want     *TSectionList
+		wantErr bool
+	}{
+		{"0", "", true},
+		{"1", fName, false},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewIni(tt.filename)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("%q: NewIni() error = %q, wantErr %v",
+					tt.name, err, tt.wantErr)
+				return
+			}
+		})
+	}
+} // TestNewIni()
+
 const cmpstring = "qwertzuiopü+#äölkjhgfdsa<yxcvbnm,.-^1234567890ß´qwertzuiop"
 
 func Benchmark_compare1(b *testing.B) {
